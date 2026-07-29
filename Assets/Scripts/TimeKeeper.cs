9cbd2dd0f3fca8cf8aa3171797c085b5e18aaf43
goroh <65319834+goroh3rd@users.noreply.github.com>
2026-07-30 05:56:19 +0900

    城を置くフェーズ実装の準備

--- Assets/Scripts/TimeKeeper.cs
+++ Assets/Scripts/TimeKeeper.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class TimeKeeper : MonoBehaviour
 {
     [SerializeField] private float _timeLimit;
+    [SerializeField] private float _phaseDuration;
     public float TimeRemaining { get; private set; }
     public float IsTimeUp => TimeRemaining <= 0f ? 1f : 0f;
     public bool RockPhase { get; private set; } = true;
@@ -22,10 +25,22 @@ public class TimeKeeper : MonoBehaviour
             if (TimeRemaining <= 0)
             {
                 TimeRemaining = 0;
-                RockPhase = false;
-                CastlePhase = true;
-                // Handle game over logic here
+                // この辺でタイムアップの音を鳴らす
+                DoWithDelay(_phaseDuration, () =>
+                {
+                    ChangeToCastlePhase();
+                });
             }
         }
+    }
+    private void ChangeToCastlePhase()
+    {
+        RockPhase = false;
+        CastlePhase = true;
+    }
+    private IEnumerator DoWithDelay(float delay, System.Action action)
+    {
+        yield return new WaitForSeconds(delay);
+        action?.Invoke();
     }
 }
